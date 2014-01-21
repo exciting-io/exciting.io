8c561102450b9480fbced27f7759784763905220
James Adam <james@lazyatom.com>
2014-01-21 17:30:54 -0600

    Randomize background while I pick one I like.

--- _assets/javascripts/main.js
+++ _assets/javascripts/main.js
@@ -4,4 +4,23 @@
 $(function() {
   $(".title h1").lettering();
   $('form.mailchimp').ajaxChimp();
+  (function() {
+    var backgrounds = ['bokeh',
+      'bricks',
+      'bridge-at-night',
+      'bridge-from-below',
+      'buildings',
+      'doors',
+      'forest',
+      'hiker',
+      'millenium-bridge',
+      'mist',
+      'mountains',
+      'phones-and-coffee',
+      'rail-bridge',
+      'shoes-in-field',
+      'window-raindrops'];
+      var selected_background = backgrounds[Math.round(Math.random()*backgrounds.length)];
+      $("body.wide header").attr('class', selected_background);
+  })();
 })
